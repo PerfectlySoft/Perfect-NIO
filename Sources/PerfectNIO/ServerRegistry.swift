6b8a9bcf8164ad0fca085e0941b2d2cfc9759685
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-06-06 12:20:47 -0400

    Removed in-progress code

--- Sources/PerfectNIO/ServerRegistry.swift
+++ Sources/PerfectNIO/ServerRegistry.swift
@@ -16,45 +16,46 @@
 //===----------------------------------------------------------------------===//
 //
 
-import Foundation
-import NIO
-import NIOHTTP1
-import NIOSSL
-import Dispatch
-
-extension SocketAddress: Hashable {
-	public func hash(into hasher: inout Hasher) {
-		hasher.combine(description.hashValue)
-	}
-}
-
-struct ServerInfo {
-	let address: SocketAddress
-	let hostName: String?
-	let channel: Channel
-	let tls: TLSConfiguration?
-	let sslContext: SSLContext?
-}
-
-final class MultiPlexHTTPHandler: ChannelInboundHandler {
-	public typealias InboundIn = HTTPServerRequestPart
-	public typealias OutboundOut = HTTPServerResponsePart
-}
-
-fileprivate enum GlobalServerMap {
-	static private let serverInfoAccessQueue = DispatchQueue(label: "serverInfoAccess")
-	static private var serverMap: [SocketAddress:ServerInfo] = [:]
-	static func makeKey(port: Int,
-						address: String) -> String {
-		return "\(address)"
-	}
-	static func get(_ key: SocketAddress) -> ServerInfo? {
-		return GlobalServerMap.serverInfoAccessQueue.sync { GlobalServerMap.serverMap[key] }
-	}
-	static func set(_ key: SocketAddress, _ newValue: ServerInfo) {
-		GlobalServerMap.serverInfoAccessQueue.sync { GlobalServerMap.serverMap[key] = newValue }
-	}
-}
+// TODO
+//import Foundation
+//import NIO
+//import NIOHTTP1
+//import NIOSSL
+//import Dispatch
+//
+//extension SocketAddress: Hashable {
+//	public func hash(into hasher: inout Hasher) {
+//		hasher.combine(description.hashValue)
+//	}
+//}
+//
+//struct ServerInfo {
+//	let address: SocketAddress
+//	let hostName: String?
+//	let channel: Channel
+//	let tls: TLSConfiguration?
+//	let sslContext: SSLContext?
+//}
+//
+//final class MultiPlexHTTPHandler: ChannelInboundHandler {
+//	public typealias InboundIn = HTTPServerRequestPart
+//	public typealias OutboundOut = HTTPServerResponsePart
+//}
+//
+//fileprivate enum GlobalServerMap {
+//	static private let serverInfoAccessQueue = DispatchQueue(label: "serverInfoAccess")
+//	static private var serverMap: [SocketAddress:ServerInfo] = [:]
+//	static func makeKey(port: Int,
+//						address: String) -> String {
+//		return "\(address)"
+//	}
+//	static func get(_ key: SocketAddress) -> ServerInfo? {
+//		return GlobalServerMap.serverInfoAccessQueue.sync { GlobalServerMap.serverMap[key] }
+//	}
+//	static func set(_ key: SocketAddress, _ newValue: ServerInfo) {
+//		GlobalServerMap.serverInfoAccessQueue.sync { GlobalServerMap.serverMap[key] = newValue }
+//	}
+//}
 
 
 
