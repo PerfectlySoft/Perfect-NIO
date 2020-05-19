9c20d9a8d80bc41b53e61f0e1577617da9ba6723
Kyle Jessup <kyle.jessup@treefrog.ca>
2020-05-19 14:53:42 -0400

    Version bumps. Fixed warning

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
 		.library(name: "PerfectNIO", targets: ["PerfectNIO"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", .branch("4.0-dev")),
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "2.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MIME.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
@@ -21,7 +21,7 @@ let package = Package(
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CZlib-src.git", from: "0.0.0"),
 		
 		// tests only
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
 	],
 	targets: [
 		.target(name: "PerfectNIOExe", dependencies: [
